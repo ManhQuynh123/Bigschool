3fb3ff288fc7b65b10f1e524c2653b6b9842429d
Admin <Admin@MSI>
2022-03-27 23:20:46 +0700

    Add project files.

---
+++ Bigschool2004/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Bigschool2004.MvcApplication" Language="C#" %>
---
+++ Bigschool2004/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Bigschool2004.Startup))]
+namespace Bigschool2004
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
